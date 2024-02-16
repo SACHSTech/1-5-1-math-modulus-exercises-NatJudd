932e40df66d727c30d43822431f94f0d786ae75f
NateJudd <nathaniel.judd25@ycdsbk12.ca>
2024-02-16 00:53:22 -0500

    solved

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,26 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * this code lets you enter a number of minutes, then will calculate the number of days, hours and minutes
+  * @author:n.judd
   */
   
   public void run() {
-    
-    // start coding here
-    
+    // declare variables
+    double dblM;
+    double dblDay;
+    double dblHour;
+    double dblMin;
+
+    // receive input
+    dblM = readDouble("Input an amount of minutes: ");
+
+    // calculate output
+    dblDay = Math.floor(dblM / 1440);
+    dblHour = Math.floor(((dblM / 1440) - dblDay) * 24);
+    dblMin = ((((dblM / 1440) - dblDay) * 24) - dblHour) * 60;
+
+    // output
+    System.out.println(dblM + " minutes equals " + dblDay + " day(s), " + dblHour + " hour(s), and " + dblMin + " minute(s)");
   }
 }
